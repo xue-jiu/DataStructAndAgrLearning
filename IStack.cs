d1ca8abae9387bf74de96375756002ed060fbfb9
xuejiu <1764343065@qq.com>
2022-07-11 07:57:21 +0800

    栈接口

---
+++ IStack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructAndAgrLearning
+{
+    public interface IStack<T>
+    {
+        public int Count { get;}
+        public bool IsEmpty { get;}
+        public T Peek();
+        public T Pop();
+        public void Push(T t);
+
+
+
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructAndAgrLearning
+{
+    public class LinkListStack<T> : IStack<T>
+    {
+        private LinkedList<T> ListStack;
+        public LinkListStack()
+        {
+
+        }
+
+        public int Count { get => ListStack.Count; }
+
+        public bool IsEmpty { get => Count == 0; }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T t)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
